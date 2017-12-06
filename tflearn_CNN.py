d0aca106977e49ca46d1c5c9c5ea471b01b9c4a3
bclai7 <bclai7@users.noreply.github.com>
2017-12-06 15:41:24 -0500

    tflearn update

--- tflearn_CNN.py
+++ tflearn_CNN.py
@@ -1,51 +1,59 @@
-import tensorflow as tf
-import cv2
-import numpy as np
-import os
-from random import shuffle
-import re
+import cv2                 # working with, mainly resizing, images
+import numpy as np         # dealing with arrays
+import os                  # dealing with directories
+from random import shuffle # mixing up or currently ordered data that might lead our network astray in training.
+from tqdm import tqdm      # a nice pretty percentage bar for tasks. Thanks to viewer Daniel BA1/4hler for this suggestion
 import tflearn
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
 tf.reset_default_graph()
 
-train_location = 'Traffic Sign Data Set/TRAIN/'
-test_location = 'Traffic Sign Data Set/TEST/'
 
-x_pixels = 50
-y_pixels = 50
+TRAIN_DIR = 'Traffic Sign Data Set/TRAIN-LARGE/'
+TEST_DIR = 'Traffic Sign Data Set/TEST/'
+IMG_SIZE = 50
+LR = 1e-3
 
-learn_rate = 1e-3
+MODEL_NAME = 'trafficSigns-{}-{}.model'.format(LR, '2conv-basic') # just so we remember which saved model is which, sizes must match
 
-model_name = 'trafficsigns-{}-{}.model'.format(learn_rate, '2conv-basic')
 
 def label_img(img):
-    word_label = img.split('_')[0]
-    # conversion to one-hot array [Do Not Enter, No Left Turn, One Way, Speed Limit, Stop, Yield]
-    if word_label == 'Do':
+    word_label = img.split('.')[-2]
+    # conversion to one-hot array [cat,dog]
+    if 'Do_Not_Enter_Sign' in word_label:
         return [1, 0, 0, 0, 0, 0]
-    elif word_label == 'No':
+    elif 'No_Left_Turn_Sign' in word_label:
         return [0, 1, 0, 0, 0, 0]
-    elif word_label == 'One':
+    elif 'One_Way_Sign' in word_label:
         return [0, 0, 1, 0, 0, 0]
-    elif word_label == 'Speed':
+    elif 'Speed_Limit_25MPH' in word_label:
         return [0, 0, 0, 1, 0, 0]
-    elif word_label == 'Stop':
+    elif 'Stop_Sign' in word_label:
         return [0, 0, 0, 0, 1, 0]
-    elif word_label == 'Yield':
+    elif 'Yield_Sign' in word_label:
         return [0, 0, 0, 0, 0, 1]
+    else:
+        return [0, 0, 0, 0, 0, 0]
+
 
 def create_train_data():
     training_data = []
-    for img in os.listdir(train_location):
-        label = label_img(img)
-        path = os.path.join(train_location,img)
-        img = cv2.imread(path)
-        img = cv2.resize(img, (x_pixels, y_pixels))
-        training_data.append([np.array(img),np.array(label)])
+    for img in tqdm(os.listdir(TRAIN_DIR)):
+        if img is not None:
+            label = label_img(img)
+            if label == [0, 0, 0, 0, 0, 0]:
+                continue
+            path = os.path.join(TRAIN_DIR, img)
+            img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+            img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+            training_data.append([np.array(img), np.array(label)])
+        else:
+            print("image data null")
+            quit()
     shuffle(training_data)
     np.save('train_data.npy', training_data)
     return training_data
@@ -53,21 +61,23 @@ def create_train_data():
 
 def process_test_data():
     testing_data = []
-    for img in os.listdir(test_location):
-        path = os.path.join(test_location, img)
+    for img in tqdm(os.listdir(TEST_DIR)):
+        path = os.path.join(TEST_DIR, img)
         img_num = img.split('.')[0]
-        img = cv2.imread(path)
-        img = cv2.resize(img, (x_pixels, y_pixels))
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
         testing_data.append([np.array(img), img_num])
 
     shuffle(testing_data)
     np.save('test_data.npy', testing_data)
     return testing_data
 
-train_data=create_train_data()
-test_data=process_test_data()
 
-convnet = input_data(shape=[None, x_pixels, y_pixels, 1], name='input')
+train_data = create_train_data()
+# If you have already created the dataset:
+# train_data = np.load('train_data.npy')
+
+convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
 
 convnet = conv_2d(convnet, 32, 5, activation='relu')
 convnet = max_pool_2d(convnet, 5)
@@ -75,46 +85,82 @@ convnet = max_pool_2d(convnet, 5)
 convnet = conv_2d(convnet, 64, 5, activation='relu')
 convnet = max_pool_2d(convnet, 5)
 
+### EXTRA LAYERS
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+###
+
 convnet = fully_connected(convnet, 1024, activation='relu')
 convnet = dropout(convnet, 0.8)
 
 convnet = fully_connected(convnet, 6, activation='softmax')
-convnet = regression(convnet, optimizer='adam', learning_rate=learn_rate, loss='categorical_crossentropy', name='targets')
+convnet = regression(convnet, optimizer='adam', learning_rate=LR,
+                     loss='categorical_crossentropy', name='targets')
 
 model = tflearn.DNN(convnet, tensorboard_dir='log')
 
+if os.path.exists('{}.meta'.format(MODEL_NAME)):
+    model.load(MODEL_NAME)
+    print('model loaded!')
 
-#############
-'''
-if os.path.exists('{}.meta'.format(model_name)):
-    model.load(model_name)
-    print('model loaded')
-'''
+train = train_data[:-500]
+test = train_data[-500:]
 
-train = train_data
-test=test_data
-
-#print(len(train))
-#print(len(test))
-
-# for i in train:
-#     print('%s' % i)
-#     print(i)
-#     print(len(i))
-
-X = np.array([i[0] for i in train]).reshape(-1,x_pixels,y_pixels,1)
+X = np.array([i[0] for i in train]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 Y = [i[1] for i in train]
 
-print('len X')
-print(len(X))
-
-test_x = np.array([i[0] for i in test]).reshape(-1,x_pixels,y_pixels,1)
+test_x = np.array([i[0] for i in test]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 test_y = [i[1] for i in test]
 
 model.fit({'input': X}, {'targets': Y}, n_epoch=3, validation_set=({'input': test_x}, {'targets': test_y}),
-    snapshot_step=500, show_metric=True, run_id=model_name)
-print ('HERE')
-model.save(model_name)
+    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+
+# model.save(MODEL_NAME)
+
+# TESTING
+test_data = process_test_data()
+fig = plt.figure()
+
+for num, data in enumerate(test_data[:12]):
+
+    img_num = data[1]
+    img_data = data[0]
+
+    y = fig.add_subplot(3, 4, num + 1)
+    orig = img_data
+    data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
+    # model_out = model.predict([data])[0]
+    model_out = model.predict([data])[0]
+
+    if np.argmax(model_out) == 0:
+        str_label = 'Do Not Enter'
+    elif np.argmax(model_out) == 1:
+        str_label = 'No Left Turn'
+    elif np.argmax(model_out) == 2:
+        str_label = 'One Way'
+    elif np.argmax(model_out) == 3:
+        str_label = 'Speed Limit 25'
+    elif np.argmax(model_out) == 4:
+        str_label = 'Stop'
+    elif np.argmax(model_out) == 5:
+        str_label = 'Yield'
+    else:
+        str_label = 'Other'
+
+    y.imshow(orig, cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+plt.show()
+
+
+
+
 
 
 
